f893ea60de1a40303800f43ec6f4b2f481d85123
Henrique Guazzelli Mendes <henrique@verifymyage.co.uk>
2022-12-14 20:43:56 -0300

    aula-2: basic http server with hello GET route

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	httpRouter := http.NewServeMux()
+
+	httpRouter.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != http.MethodGet {
+			writer.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		fmt.Printf("Method being called -> " + request.Method)
+		writer.WriteHeader(201)
+		_, _ = writer.Write([]byte("Hello World!"))
+	})
+
+	err := http.ListenAndServe(":8000", httpRouter)
+	if err != nil {
+		log.Fatalf("error to init server at PORT :8000. Error: %s", err.Error())
+	}
 }
